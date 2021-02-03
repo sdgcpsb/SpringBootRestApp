bb12037a6b3a9eae8537162afc85eecfaf5eea1d
rakshita-singh02 <75852987+rakshita-singh02@users.noreply.github.com>
2021-02-03 23:08:33 +0530

    Update SpringBootRestAppApplicationTests.java

--- src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
+++ src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -1,15 +1,1 @@
-/* package com.kkjavatutorials.SpringBootRestApp;
-
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootRestAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
 
